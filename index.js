bea3d4c637c95f1e44fb49d6e6afe49979b66302
Shwathy <114863087+Shwathy@users.noreply.github.com>
2022-10-06 20:36:04 +0530

    update

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ items[1].textContent='hello 2';
 items[1].style.fontWeight='bold';
 items[1].style.backgroundColor='yellow';
 items.style.backgroundColor='#f4f4f4';
+//tagname
+//var items=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent='hello 2';
+//li[1].style.fontWeight='bold';
+//li[1].style.backgroundColor='yellow'
+//query selector
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+var input=document.querySelector('input');
+input.value ="hello world";
